7bb7db8681aa291859dc36e8187beb35ecc175bd
rkovach24eu <r.kovach@e-u.edu.ua>
2024-11-13 18:08:03 +0200

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,23 @@ mod chapter3;
 mod chapter4;
 mod chapter5;
 mod chapter6;
+mod chapter7;
+mod chapter8;
+mod chapter9;
+mod chapter10;
+mod chapter11;
+mod practice4;
+mod practice5;
+mod practice6;
+mod practice7;
+mod practice8;
+mod practice9;
+mod practice10;
+mod practice11;
+mod practice12;
+mod practice13;
+mod practice14;
+mod practice16;
 
 fn main() {
     println!("Hello, world!");
