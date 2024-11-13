e94b4a0959991f745afb987a8acc4cec81d5f481
rkovach24eu <r.kovach@e-u.edu.ua>
2024-11-13 17:15:11 +0200

    Update practice4.rs

--- src/practice4.rs
+++ src/practice4.rs
@@ -1,1 +1,35 @@
+const SIZE: usize = 5;
+
+fn main() {
+    let mut output = String::new();
+    
+    // Верхня частина ромба
+    for i in 0..SIZE {
+        // Додаємо пробіли перед зірочками
+        for _ in 0..(SIZE - i - 1) {
+            output.push(' ');
+        }
+        // Додаємо зірочки з пробілами
+        for _ in 0..(2 * i + 1) {
+            output.push('*');
+        }
+        output.push('\n');
+    }
+    
+    // Нижня частина ромба
+    for i in (0..SIZE - 1).rev() {
+        // Додаємо пробіли перед зірочками
+        for _ in 0..(SIZE - i - 1) {
+            output.push(' ');
+        }
+        // Додаємо зірочки з пробілами
+        for _ in 0..(2 * i + 1) {
+            output.push('*');
+        }
+        output.push('\n');
+    }
+    
+    // Виводимо результат одним print!
+    print!("{}", output);
+}
 
